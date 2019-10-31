97336ff37d503058311df8f12abac79b03585d6d
hejianfang <hejf@lexueguo.com>
2019-10-31 18:10:27 +0800

    管理端加入新的页面

---
+++ src/pages/admin/views/login/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="Login">
+    vue模板
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {},
+  components: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  created () {},
+  methods: {},
+  filters: {},
+  watch: {}
+}
+</script>
+
+<style lang="less">
+  .Login{}
+</style>
